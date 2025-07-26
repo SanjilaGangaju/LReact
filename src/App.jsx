ef1bd644ed6af8277832f80b30faebbcd13889db
Sanjila Gangaju <canzilagangaju@gmail.com>
2025-07-26 14:05:52 +0545

    'react-icons install

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,1 @@
-
-import Person from "./components/person";
-import Product from "./components/Product";
-
-const App=()=>{
-  return (
-  <div>
-    <Person name="Sanjila" age="22"></Person>
-    <Product name="macbook air m4" price="$100"></Product>
-  </div>
-  )
-}
-export default App;
+import './index.css';
--- src/components/Greet.jsx
+++
@@ -1,4 +0,0 @@
-const Greet= ()=>{
-    return <h1>Good Morning</h1>
-}
-export default Greet;
