0248377f089f9d4420de18fed616705f68ecb441
Sanjila Gangaju <canzilagangaju@gmail.com>
2025-07-27 12:37:16 +0545

    added score system

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,44 @@ const App = () => {
   const[score, setScore]=useState(0)
   const[currentQuestion, setCurrentQuestion] = useState(0)
   const [showResult, setShowResult] = useState(0)
-  const [message, setMessage] = useState("Answering");
+  const [message, setMessage] = useState("Answering.........");
+  const [correct, setCorrect] = useState("wrong");
 
   const handleOption=(optionValue)=>{
    
     if (optionValue===quizQuestions[currentQuestion].answer){
        setMessage("Correct")
+       setCorrect("correct");
+       
+       setScore(score+1);
+      
     }
     else{
       setMessage("wrong")
     }
   }
+  const handleNextQuestion=()=>{
+    
+    setCorrect("wrong");
+    setCurrentQuestion(currentQuestion+1)
+    setMessage("Answering........")
+    
+    
+
+   }
+   const handlePrevQuestion=()=>{
+    setCurrentQuestion(currentQuestion-1)
+
+   }
+   const handleRestart=()=>{
+    setScore(0);
+    setCurrentQuestion(0)
+    setMessage("Answering.......")
+    
+   }
   return (
     <div>
-      <QuizzesDisplay questions={quizQuestions} message={message} currentQuestion={currentQuestion} handleOption={handleOption}></QuizzesDisplay>
+      <QuizzesDisplay questions={quizQuestions} score={score}message={message} correct={correct}currentQuestion={currentQuestion} handleOption={handleOption} handleNextQuestion={handleNextQuestion} handleRestart={handleRestart}handlePrevQuestion={handlePrevQuestion}></QuizzesDisplay>
     </div>
   )
 }
--- src/QuizzesDisplay.jsx
+++ src/QuizzesDisplay.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 
-const QuizzesDisplay = ({ questions ,message, currentQuestion, handleOption}) => {
+const QuizzesDisplay = ({ questions ,message, currentQuestion, handleOption, handleNextQuestion, handlePrevQuestion, score, correct, handleRestart}) => {
  
   return (
     <div>
       <p>{questions[currentQuestion].question}</p>
-      {questions[currentQuestion].options.map(option=><button key={option} onClick={()=>handleOption(option)}>{option}</button>)}
-      <p>You are absolutely {message}!!!!</p>
+      {questions[currentQuestion].options.map(option=><button key={option} disabled={correct==="correct"? true : false} onClick={()=>handleOption(option)}>{option}</button>)}<br></br>
+      <p>{message}!!!!</p>
+      <button onClick={()=>handleNextQuestion()} 
+        disabled={currentQuestion==questions.length-1? true: false}>Next</button><br></br><br></br>
+      <button onClick={()=>handlePrevQuestion()} 
+        disabled={currentQuestion>0?false: true}>Previous</button>
+        <h1>Score:{score}</h1>
+      <button onClick={()=>handleRestart()}>Restart</button>
     </div>
   )
 }
